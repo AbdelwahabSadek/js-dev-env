86668d8fa7cf8464af13405840f74ed503894ed2
AbdelwahabSadek <Abdelwahab.Sadek@gmail.com>
2022-10-17 20:53:36 +0200

    adds unit test examples

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,20 @@
-import {expect} from 'chai';
+import {expect} from "chai";
+import jsdom from "jsdom";
+import fs from "fs";
 
-describe('t1', () =>{
-  it('should pass', () => {
+describe("t1", () =>{
+  it("should pass", () => {
     expect(true).to.equal(true);
   })
 })
+
+describe("index.html", ()=>{
+  it("should say hello adam", ()=>{
+    const index = fs.readFileSync("./src/index.html", "utf-8");
+    const {JSDOM} = jsdom;
+    const dom = new JSDOM(index);
+    const h1 = dom.window.document.getElementById("hello-header");
+    expect(h1.innerHTML).to.equal("Hello Adam!");
+    dom.window.close();
+  })
+});
